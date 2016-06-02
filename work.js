38ec9430aa726bf7884e57eb1c37873f072ba623
Dmytro Kulyk <dmytrokulyk@yahoo.com>
2016-06-02 13:17:29 -0700

    Add responsive

--- work.js
+++ work.js
@@ -13,7 +13,7 @@ imgHover("#facebookIcon", "images/icons/svg/Facebook.svg", "images/icons/svg/Fac
 imgHover("#linkedinIcon", 'images/icons/svg/Linkedin.svg', "images/icons/svg/Linkedin-black.svg" );
 imgHover("#letterIcon", 'images/icons/svg/envelope.svg', "images/icons/svg/letter.svg" );
 
-
+//============Scrolling==============
 
 $('a[href^="#"]').on('click', function(event) {
 
@@ -24,15 +24,43 @@ $('a[href^="#"]').on('click', function(event) {
         $('html, body').animate({
             scrollTop: target.offset().top
         }, 1000);
-    }
+    
+    // $('#example-one a').removeClass('current_page_item');
+    // $(this).addClass('current_page_item');
+}
+});
 
-    });
+ //=============TextAnimation===========  
 
 $(".main h1").textillate({
-    in: {effect: "fadeIn", delay:100},
+    in: {effect: "fadeIn", delay: 70},
+});
+
+//==============NavBarButton=======
+ // $('button').click(function(){ 
+ // $('.navHeaderCollapse').toggle('slow');
+   
+ //   });
+
 });
 
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
